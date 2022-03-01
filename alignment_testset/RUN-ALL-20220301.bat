0dbc64d79a8d00908065144d831e324f080f1d73
Peter (RDF Ltd.) <peter.bonsma@rdf.bg>
2022-03-01 19:26:36 +0200

    Add files via upload

---
+++ alignment_testset/RUN-ALL-20220301.bat
@@ -0,0 +1,26 @@
+call 00-HorizontalAlignment-BlossCurve-20220131.bat
+call 01-HorizontalAlignment-CircularArc-20220131.bat
+call 02-HorizontalAlignment-Clothoid-20220131.bat
+call 03-HorizontalAlignment-CosineCurve-20220131.bat
+call 04-HorizontalAlignment-Cubic-20220131.bat
+call 05-HorizontalAlignment-HelmertCurve-20220131.bat
+call 06-HorizontalAlignment-Line-20220131.bat
+call 07-HorizontalAlignment-SineCurve-20220131.bat
+call 08-HorizontalAlignment-VienneseBend-20220131.bat
+
+call 10-VerticalAlignment-CircularArc-20220131.bat
+call 11-VerticalAlignment-Clothoid-20220131.bat
+call 12-VerticalAlignment-ConstantGradient-20220131.bat
+call 13-VerticalAlignment-ParabolicArc-20220131.bat
+
+call 20-CantAlignment-BlossCurve-20220301.bat
+call 21-CantAlignment-ConstantCant-20220301.bat
+call 22-CantAlignment-CosineCurve-20220301.bat
+call 23-CantAlignment-HelmertCurve-20220301.bat
+call 24-CantAlignment-LinearTransition-20220301.bat
+call 25-CantAlignment-SineCurve-20220301.bat
+call 26-CantAlignment-VienneseBend-20220301.bat
+
+call 30-Extra-CantAlignment-VienneseBend-20220131.bat
+
+pause:
